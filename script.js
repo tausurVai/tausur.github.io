cd2c9e4e5f142c435a29ef98daad2f917283e84d
tausurVai <cranecocplayer@gmail.com>
2026-04-23 19:36:19 +0600

    3rd

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀" : "☾";
+});
